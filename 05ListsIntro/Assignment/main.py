2240f448d303c79ce93c9947fef93876062ece9f
Kaileb Longbrake <25kaileblongbrake@usd266.com>
2023-11-27 10:24:32 -0600

    finish most of assignment

---
+++ 05ListsIntro/Assignment/main.py
@@ -0,0 +1,37 @@
+def make_abc():
+    return ["a", "b", "c"]
+
+print("--->", make_abc())
+
+
+
+
+
+def equal_edges(list):
+    if list[0] == list[-1]:
+        return True
+    else:
+        return False
+    
+print("Equal Edges -->", equal_edges([1, 2, 3, 4, 1]))
+print("Equal Edges -->", equal_edges([5, 6, 7, 8, 9]))
+print("Equal Edges -->", equal_edges([-1, 0, 1, 2, -1]))
+print("Equal Edges -->", equal_edges([4, 4]))
+
+
+
+
+
+def common_edge(list1, list2):
+    if list1[0] == list2[0] or list1[0] == list2[-1]:
+        return True
+    elif list1[-1] == list2[0] or list1[-1] == list2[-1]:
+        return True
+    else:
+        return False
+    
+print("Common edge ---->", common_edge([1, 2, 3 ,4], [5, 6, 7, 8]))
+print("Common edge ---->", common_edge([1, 2, 3], [3, 4, 5]))
+print("Common edge ---->", common_edge([4, 5, 6], [7, 8, 9]))
+print("Common edge ---->", common_edge([-1, 0, 1, 2, -1], [2, 3, 4, 5]))
+print("Common edge ---->", common_edge([3, 3, 3], [3, 3, 3, ]))
