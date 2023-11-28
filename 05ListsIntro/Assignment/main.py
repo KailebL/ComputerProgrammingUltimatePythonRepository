7fca048a59cf98e16b4d749b8855fb094c4f2d02
Kaileb Longbrake <25kaileblongbrake@usd266.com>
2023-11-28 10:09:21 -0600

    Finished assignment 5

--- 05ListsIntro/Assignment/main.py
+++ 05ListsIntro/Assignment/main.py
@@ -40,7 +40,90 @@ print("Common edge ---->", common_edge([3, 3, 3], [3, 3, 3, ]))
 
 
 def all_the_same(list):
-    list = list.replace(" ", "")
-    n1, n2, n3 = int(list.split(","))
+    if len(list) == 3:
+        if list[0] == list[1] and list[1] == list[2]:
+            return True
+        else:
+            return False
+    else:
+        return "List must have 3 values"
 
-print(all_the_same)
+print("All the same ---->", all_the_same([1, 2, 3]))
+print("All the same ---->", all_the_same([5, 5, 5]))
+print("All the same ---->", all_the_same([-1, 0, 1]))
+print("All the same ---->", all_the_same([3, 3, 3]))
+print("All the same ---->", all_the_same([4, 5, 6]))
+
+
+
+
+def all_unique(numberslist):
+    if len(numberslist) == 3:
+        if numberslist[0] != numberslist[1] and numberslist[1] != numberslist[2]:
+            return True
+        elif numberslist[0] == numberslist[1] and numberslist[1] == numberslist[2]:
+            return False
+        else:
+            return False
+    else:
+        return "List must have 3 values"
+    
+print("All unique ---->", all_unique([1, 2, 3]))
+print("All unique ---->", all_unique([5, 5, 5]))
+print("All unique ---->", all_unique([-1, 0, 1]))
+print("All unique ---->", all_unique([3, 3, 3]))
+print("All unique ---->", all_unique([4, 5, 6]))
+
+
+
+
+def increasing(list):
+    if len(list) == 3:
+        if list[0] < list[1] and list[1] < list[2]:
+            return True
+        else:
+            return False
+    else:
+        return "List must have 3 values"
+    
+print("Increasing ---->", increasing([1, 2, 3]))
+print("Increasing ---->", increasing([5, 5, 5]))
+print("Increasing ---->", increasing([-1, 0, 1]))
+print("Increasing ---->", increasing([3, 3, 3]))
+print("Increasing ---->", increasing([4, 5, 6]))
+
+
+
+
+def all_true(booleans):
+    if len(booleans) == 3:
+        if booleans[0] == True and booleans[0] == booleans[1] and booleans[1] == booleans[2]:
+            return True
+        else:
+            return False
+    else:
+        return "List must have 3 values"
+    
+print("All true ---->", all_true([True, False, True]))
+print("All true ---->", all_true([False, False, False]))
+print("All true ---->", all_true([True, True, True]))
+print("All true ---->", all_true([False, True, False]))
+print("All true ---->", all_true([True, False, True]))
+
+
+
+
+def mostly_true(list):
+    if len(list) == 3:
+        if list.count(True) >= 2:
+            return True
+        else:
+            return False
+    else:
+        return "List must have 3 values"
+    
+print("Mostly true ---->", mostly_true([True, False, True]))
+print("Mostly true ---->", mostly_true([False, False, False]))
+print("Mostly true ---->", mostly_true([True, True, True]))
+print("Mostly true ---->", mostly_true([False, True, False]))
+print("Mostly true ---->", mostly_true([True, False, False]))
